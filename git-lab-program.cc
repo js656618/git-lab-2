42f3295518bb8758b38e081fe97885172039fca3
unknown <js656618@ohio.edu>
2021-01-29 17:30:55 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Justin Spires>
+ *        Date: <January 29 2021>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    return 0;
+    cout << "Hello Git!!" << endl;
+    
+}// main
